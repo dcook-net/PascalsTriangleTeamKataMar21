6604dc1bf445c4e2b306350c724c6fe87df5cf0b
oliwennell <oli.wennell@gmail.com>
2021-03-19 14:58:17 +0000

    :sparkles: Getting there...

--- PascalsTriangle/Class1.cs
+++ PascalsTriangle/Class1.cs
@@ -11,8 +11,34 @@ namespace PascalsTriangle
            return Enumerable
                .Range(0, (int) rows)
                .Aggregate(new List<uint>()
-                   .AsEnumerable(),fold);
-                
+                   .AsEnumerable(), fold);
+
+        }
+
+        private static IEnumerable<uint> fold(IEnumerable<uint> triangle, int currentRowNumber)
+        {
+            if (currentRowNumber == 0)
+                return new List<uint>(){ 1 };
+
+            var previousRow = triangle.Take(currentRowNumber).ToArray();
+            // var lastRowThing = triangle.IndexOf(triangle.Count() - 1) + 1
+            //  = rowNumber - 1 
+            //var newRow = new uint[] { 1, (previousRow[0] + previousRow[1]) + ... + 1
+            var newRow = new List<uint>();
+            newRow.Add(1);
+            if (previousRow.Length >= 2)
+            {
+                for (uint i = 0; i < currentRowNumber - 1; i++)
+                {
+                    var newThing = previousRow[i] + previousRow[i+1];
+                    newRow.Add(newThing);
+                }
+            }
+            newRow.Add(1);
+
+            var thingy = triangle.Concat(newRow);
+            return thingy;
+
             // return rows switch
             // {
             //     0 => new uint[0],
@@ -20,12 +46,6 @@ namespace PascalsTriangle
             //     2 => new uint[] {1, 1, 1},
             //     _ => new uint[0]
             // };
-            
-        }
-
-        private static IEnumerable<uint> fold(IEnumerable<uint> accumulator, int current)
-        {
-            return accumulator;
         }
     }
 }
--- PascalsTriangleTests/UnitTests.cs
+++ PascalsTriangleTests/UnitTests.cs
@@ -11,11 +11,11 @@ namespace PascalsTriangleTests
         {
         }
 
-        [TestCase((uint)1, new uint[] {1})]
-        [TestCase((uint)2, new uint[] {1, 1, 1})]
+        // [TestCase((uint)1, new uint[] {1})]
+        // [TestCase((uint)2, new uint[] {1, 1, 1})]
         [TestCase((uint)5, new uint[]
         {
-                1, 
+                1,
                1, 1,
               1, 2, 1, 
              1, 3, 3, 1,
@@ -24,15 +24,15 @@ namespace PascalsTriangleTests
         public void ReturnsArrayOf1WhenGiven1Row(uint rows, uint[] expected)
         {
             var result = Triangle.DrawTriangle(rows);
-            
+
             Assert.That(result, Is.EqualTo(expected));
         }
-        
+
         [Test]
         public void ReturnsArrayOf0WhenGiven0Rows()
         {
             var result = Triangle.DrawTriangle(0);
-            
+
             Assert.That(result, Is.Empty);
         }
     }
