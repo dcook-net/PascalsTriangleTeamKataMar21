2e30e7e02bfc632acb1082881296f3b16ebc5394
dcook-net <david.cook@bglgroup.co.uk>
2021-03-22 13:05:46 +0000

    Cookies implementation

---
+++ PascalsTriangle/Triangle.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using static LanguageExt.Prelude;
+
+namespace PascalsTriangle
+{
+    public static class Triangle
+    {
+        public static IEnumerable<uint> DrawTriangle(uint rows) =>
+            Enumerable
+                .Range(0, (int) rows)
+                .Aggregate(EmptyList, BuildTriangle);
+
+        private static IEnumerable<uint> BuildTriangle(IEnumerable<uint> triangle, int currentRowNumber)
+        {
+            return (triangle is null, currentRowNumber) switch
+            {
+                (true, _) => triangle,
+                (_, 0) => FirstRow,
+                (_, 1) => SecondRow,
+                (_, _) => BuildNextRow(triangle, currentRowNumber)
+            };
+
+            static IEnumerable<uint> BuildNextRow(IEnumerable<uint> triangle, int previousRowNumber) =>
+                triangle
+                    .TakeLast(previousRowNumber)
+                    .Pairwise()
+                    .Aggregate(triangle.Append(One), (accumulator, pair) => accumulator.Append(pair.Previous + pair.Current))
+                    .Append(One);
+        }
+
+        private const uint One = 1;
+        private static IEnumerable<uint> EmptyList => List<uint>().AsEnumerable();
+        private static IEnumerable<uint> FirstRow => List(One);
+        private static IEnumerable<uint> SecondRow => List<uint>(1, 1, 1);
+    }
+}
--- PascalsTriangleTests/UnitTests.cs
+++ PascalsTriangleTests/UnitTests.cs
@@ -1,18 +1,17 @@
-using System.Linq;
+using System.Collections.Generic;
 using NUnit.Framework;
 using PascalsTriangle;
+using Shouldly;
+using MoreLinq;
 
 namespace PascalsTriangleTests
 {
     public class Tests
     {
-        [SetUp]
-        public void Setup()
-        {
-        }
-
-        // [TestCase((uint)1, new uint[] {1})]
-        // [TestCase((uint)2, new uint[] {1, 1, 1})]
+        [TestCase((uint)1, new uint[] {1})]
+        [TestCase((uint)2, new uint[] {1, 1, 1})]
+        [TestCase((uint)3, new uint[] {1, 1, 1, 1, 2, 1})]
+        [TestCase((uint)4, new uint[] {1, 1, 1, 1, 2, 1, 1, 3, 3, 1})]
         [TestCase((uint)5, new uint[]
         {
                 1,
@@ -21,11 +20,25 @@ namespace PascalsTriangleTests
              1, 3, 3, 1,
             1, 4, 6, 4, 1
         })]
+        [TestCase((uint)10, new uint[]
+        {
+            //                                          ROW
+                             1,                         //0
+                           1, 1,                        //1
+                         1, 2, 1,                       //2
+                       1,  3, 3, 1,                     //3
+                     1, 4,  6, 4, 1,
+                   1, 5, 10, 10, 5, 1,
+                 1, 6, 15, 20, 15, 6, 1,
+                1, 7, 21, 35, 35, 21, 7, 1,
+              1, 8, 28 , 56, 70, 56, 28, 8, 1,
+            1, 9 , 36, 84, 126, 126, 84, 36, 9, 1 
+        })]
         public void ReturnsArrayOf1WhenGiven1Row(uint rows, uint[] expected)
         {
             var result = Triangle.DrawTriangle(rows);
 
-            Assert.That(result, Is.EqualTo(expected));
+            result.ShouldBe(expected);
         }
 
         [Test]
@@ -34,6 +47,17 @@ namespace PascalsTriangleTests
             var result = Triangle.DrawTriangle(0);
 
             Assert.That(result, Is.Empty);
+        }
+
+        [Test]
+        public void ShouldPairWise()
+        {
+            var input = new List<int> {1, 2, 3};
+            var expected = new List<(int, int)> {(1, 2), (2, 3)};
+            
+            var result = input.Pairwise((i, i1) => (i, i1));
+            
+            Assert.That(result, Is.EqualTo(expected));
         }
     }
 }
